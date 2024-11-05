f3dc16af86b0d51c72ffa36d109c9979f62aba76
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-05 21:52:53 +0330

    kj

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
 let currentPage = 1;
 const cardsPerPage = 5;
 
-// تنظیم کوکی
-function setCookie(name, value, hours) {
-    let expires = "";
-    if (hours) {
-        const date = new Date();
-        date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
 
-// خواندن کوکی
-function getCookie(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-// حذف کوکی
-function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
-}
 
 window.onload = function() {
     const isLoggedIn = getCookie('isLoggedIn');
