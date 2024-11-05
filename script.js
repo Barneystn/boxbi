1f22515d5c48d65e02b830ef6f20e8ea6b46ed16
Barneystn <118664069+Barneystn@users.noreply.github.com>
2024-11-05 21:42:50 +0330

    kj

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
+let currentPage = 1;
+const cardsPerPage = 5;
+
+// تنظیم کوکی
+function setCookie(name, value, hours) {
+    let expires = "";
+    if (hours) {
+        const date = new Date();
+        date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// خواندن کوکی
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// حذف کوکی
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+window.onload = function() {
+    const isLoggedIn = getCookie('isLoggedIn');
+    if (isLoggedIn === 'true') {
+        document.body.style.display = 'block';
+
+        const skeletons = document.getElementById('skeletons');
+        skeletons.style.display = 'none';
+
+        const cards = document.querySelectorAll('.card');
+        cards.forEach(card => {
+            card.style.display = 'block';
+        });
+
+        const params = new URLSearchParams(window.location.search);
+        const pageParam = params.get('page');
+        currentPage = pageParam ? parseInt(pageParam) : 1;
+        
+        showPage(currentPage);
+    } else {
+        window.location.href = '../login.html';
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     const cards = document.querySelectorAll("#movie-list .card");
     const currentPath = window.location.pathname;
@@ -54,3 +108,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     toggleSearchButton(); // غیرفعال کردن دکمه جستجو در ابتدا
 });
+
+// رویدادها
+document.querySelector('#search-input').addEventListener('input', filterMovies);
+document.querySelector('#search-input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        filterMovies();
+    }
